4ca6c17d43119f51d95c88b000aacaccea331d89
Alex Story <astory11@yahoo.com>
2023-12-01 11:38:20 -0600

    Finished Day 1

---
+++ 2023/Day1/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+open System.IO
+
+let replaceDigits (s: string) =
+    s.Replace("one", "o1e")
+    |> _.Replace("two", "t2o")
+    |> _.Replace("three", "t3e")
+    |> _.Replace("four", "f4r")
+    |> _.Replace("five", "f5e")
+    |> _.Replace("six", "s6x")
+    |> _.Replace("seven", "s7n")
+    |> _.Replace("eight", "e8t")
+    |> _.Replace("nine", "n9e")
+
+let input =
+    Path.Join(__SOURCE_DIRECTORY__, "input.txt")
+    |> File.ReadAllLines // ["1bct2", ...]
+    |> Seq.map (String.collect (fun c -> if Char.IsDigit(c)  then c.ToString() else "" )) // ["12", "38" ...]
+    |> Seq.map _.ToCharArray()
+    |> Seq.map (fun x -> (Array.head x |> _.ToString()) + (Array.last x |> _.ToString()))
+    |> Seq.map  int
+
+let input2 =
+    Path.Join(__SOURCE_DIRECTORY__, "input.txt")
+    |> File.ReadAllLines // ["1bct2", ...]
+    |> Seq.map replaceDigits
+    |> Seq.map (String.collect (fun c -> if Char.IsDigit(c)  then c.ToString() else "" )) // ["12", "38" ...]
+    |> Seq.map (_.ToCharArray())
+    |> Seq.map (fun x -> (Array.head x |> _.ToString()) + (Array.last x |> _.ToString()))
+    |> Seq.map int
+
+printfn $"Part 1: %d{Seq.sum input}"
+printfn $"Part 2: %d{Seq.sum input2}"
